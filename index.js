e10674b9ee743c1a2efd5465a7eb3add71ba4162
Samuel Kinyua <samwakash@gmail.com>
2024-07-22 12:30:51 +0300

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Convert response to JSON
+    })
+    .then(data => {
+      console.log('Data fetched:', data); // Log the fetched data
+      renderBooks(data); // Pass the JSON data to renderBooks
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
 }
 
 function renderBooks(books) {
+  // To pass the tests, don't forget to return your render!
+  return books.map(function (book){
+    return `<li>` + book.name +`</li>`
+    }).join('');
+
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -13,5 +31,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+ 
   fetchBooks();
 });
